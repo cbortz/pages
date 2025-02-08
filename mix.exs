3eb2ec1b5847c5cd4908b94467e27138e3667006
Chet Bortz <chet@bortz.co>
2025-02-08 01:33:50 -0500

    allow phoenix_live_view ~> 1.0

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule Pages.MixProject do
       {:mix_test_interactive, "~> 2.0", only: :dev, runtime: false},
       {:moar, "~> 1.10"},
       {:phoenix, "~> 1.6"},
-      {:phoenix_live_view, "~> 0.16.4 or ~> 0.17"}
+      {:phoenix_live_view, "~> 0.16.4 or ~> 0.17 or ~> 1.0"}
     ]
   end
 
